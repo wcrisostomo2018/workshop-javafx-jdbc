82852a97ccc1371117d831930dbb94b2c801ef9e
Willian Bento <wbento2010@gmail.com>
2020-11-06 19:45:04 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	
@@ -62,5 +62,4 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
 }
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,17 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll () {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1,"Informatica"));
-		list.add(new Department(2,"Eletronicos"));
-		list.add(new Department(3,"Moveis"));
-		return list;
+		return dao.findAll();
 	}
 }
